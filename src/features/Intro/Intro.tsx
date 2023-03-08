5ecf51a6df09737b950aff3bf90f029219f246df
Ángel <angelmonclova@gmail.com>
2023-03-08 15:43:46 +0100

    Add css styles to Intro component

---
+++ src/features/Intro/Intro.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Intro.css';
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <p>
+        Stoicism is an ancient philosophy that originated in ancient Greece and
+        remains relevant today. Stoics believe in the importance of living
+        according to reason and virtue, and in the idea that suffering and
+        negative emotions can be overcome through self-discipline and wisdom.
+        One of the fundamental ideas of Stoicism is that we cannot control what
+        happens in the external world, but we can control our response to those
+        events. Stoics believe that we can choose how we react to the things
+        that happen to us, and that we can learn to be happy even in the most
+        difficult situations.
+      </p>
+    </div>
+  );
+};
+
+export default Intro;
